c7648540f3d7e000ec1e53241cb23e5ece514f99
Alfonso Ariza <aarizaq_m@hotmail.com>
2023-02-06 20:07:33 +0100

    Add csma protocol to lora.

---
+++ src/inet/flora/lorabase/CsmaCaMacLora.cc
@@ -0,0 +1,113 @@
+//
+// Copyright (C) 2016 OpenSim Ltd.
+//
+// SPDX-License-Identifier: LGPL-3.0-or-later
+//
+
+
+#include "inet/flora/lorabase/CsmaCaMacLora.h"
+#include "inet/flora/lorabase/LoRaMacFrame_m.h"
+#include "inet/common/ModuleAccess.h"
+#include "inet/common/ProtocolGroup.h"
+#include "inet/common/ProtocolTag_m.h"
+#include "inet/common/checksum/EthernetCRC.h"
+#include "inet/common/packet/chunk/ByteCountChunk.h"
+#include "inet/linklayer/common/InterfaceTag_m.h"
+#include "inet/linklayer/common/MacAddressTag_m.h"
+#include "inet/flora/lorabase/LoRaTagInfo_m.h"
+
+namespace inet {
+namespace flora {
+
+using namespace inet::physicallayer;
+
+Define_Module(CsmaCaMacLora);
+
+
+/****************************************************************
+ * Initialization functions.
+ */
+void CsmaCaMacLora::initialize(int stage)
+{
+    CsmaCaMac::initialize(stage);
+    if (stage == INITSTAGE_LINK_LAYER) {
+        // subscribe for the information of the carrier sense
+       // search the bit app module to read the paramters.
+
+    }
+}
+
+bool CsmaCaMacLora::isAck(Packet *frame)
+{
+    const auto& macHeader = frame->peekAtFront<LoRaMacFrame>();
+    return macHeader->getType() == LORA_ACK;
+}
+
+bool CsmaCaMacLora::isBroadcast(Packet *frame)
+{
+    const auto& macHeader = frame->peekAtFront<LoRaMacFrame>();
+    return macHeader->getReceiverAddress().isBroadcast();
+}
+
+bool CsmaCaMacLora::isForUs(Packet *frame)
+{
+    const auto& macHeader = frame->peekAtFront<LoRaMacFrame>();
+    return macHeader->getReceiverAddress() == networkInterface->getMacAddress();
+}
+
+void CsmaCaMacLora::encapsulate(Packet *msg)
+{
+    auto frame = makeShared<LoRaMacFrame>();
+    frame->setChunkLength(B(headerLength));
+    auto tag = msg->getTag<LoRaTag>();
+
+    frame->setLoRaTP(tag->getPower().get());
+    frame->setLoRaCF(tag->getCenterFrequency());
+    frame->setLoRaSF(tag->getSpreadFactor());
+    frame->setLoRaBW(tag->getBandwidth());
+    frame->setLoRaCR(tag->getCodeRendundance());
+    frame->setSequenceNumber(sequenceNumber);
+    frame->setReceiverAddress(msg->getTag<MacAddressReq>()->getDestAddress());
+    frame->setTransmitterAddress(networkInterface->getMacAddress());
+
+    ++sequenceNumber;
+    //frame->setLoRaUseHeader(cInfo->getLoRaUseHeader());
+    frame->setLoRaUseHeader(tag->getUseHeader());
+    msg->insertAtFront(frame);
+}
+
+void CsmaCaMacLora::decapsulate(Packet *frame)
+{
+    auto loraHeader = frame->popAtFront<LoRaMacFrame>();
+    frame->addTagIfAbsent<MacAddressInd>()->setSrcAddress(loraHeader->getTransmitterAddress());
+    frame->addTagIfAbsent<MacAddressInd>()->setDestAddress(loraHeader->getReceiverAddress());
+    frame->addTagIfAbsent<InterfaceInd>()->setInterfaceId(networkInterface->getInterfaceId());
+    //    auto payloadProtocol = ProtocolGroup::ethertype.getProtocol(loraHeader->getNetworkProtocol());
+    //    frame->addTagIfAbsent<DispatchProtocolReq>()->setProtocol(payloadProtocol);
+    //    frame->addTagIfAbsent<PacketProtocolTag>()->setProtocol(payloadProtocol);
+}
+
+
+void CsmaCaMacLora::handleUpperPacket(Packet *packet)
+{
+    auto tagAddr = packet->findTagForUpdate<MacAddressReq>();
+    if (tagAddr == nullptr) {
+        tagAddr = packet->addTag<MacAddressReq>();
+        tagAddr->setDestAddress(MacAddress::BROADCAST_ADDRESS);
+    }
+    if (tagAddr->getDestAddress().isUnspecified()) {
+        tagAddr->setDestAddress(MacAddress::BROADCAST_ADDRESS);
+    }
+
+    EV << "frame " << packet << " received from higher layer, receiver = " << tagAddr->getDestAddress() << endl;
+    encapsulate(packet);
+    if (currentTxFrame != nullptr)
+        throw cRuntimeError("Model error: incomplete transmission exists");
+    currentTxFrame = packet;
+    handleWithFsm(currentTxFrame);
+}
+
+}
+
+} // namespace inet
+
---
+++ src/inet/flora/lorabase/CsmaCaMacLora.h
@@ -0,0 +1,54 @@
+//
+// Copyright (C) 2016 OpenSim Ltd.
+//
+// SPDX-License-Identifier: LGPL-3.0-or-later
+//
+
+
+#ifndef __INET_CSMACAMACLORA_H
+#define __INET_CSMACAMACLORA_H
+
+#include "inet/common/FSMA.h"
+#include "inet/common/packet/Packet.h"
+#include "inet/linklayer/base/MacProtocolBase.h"
+#include "inet/linklayer/contract/IMacProtocol.h"
+#include "inet/linklayer/csmaca/CsmaCaMacHeader_m.h"
+#include "inet/physicallayer/wireless/common/contract/packetlevel/IRadio.h"
+#include "inet/queueing/contract/IActivePacketSink.h"
+#include "inet/queueing/contract/IPacketQueue.h"
+#include "inet/linklayer/csmaca/CsmaCaMac.h"
+
+namespace inet {
+namespace flora {
+
+
+class INET_API CsmaCaMacLora : public inet::CsmaCaMac
+{
+protected:
+    cModule *simpleApp = nullptr;
+    uint64_t sequenceNumber = 0;
+  public:
+    /**
+     * @name Construction functions
+     */
+    //@{
+    virtual ~CsmaCaMacLora();
+
+    //@}
+  protected:
+    virtual void initialize(int stage) override;
+    virtual void handleUpperPacket(Packet *packet) override;
+    virtual void computeBitRate();
+    virtual void encapsulate(Packet *frame) override;
+    virtual void decapsulate(Packet *frame)  override;
+    virtual bool isForUs(Packet *frame) override;
+    virtual bool isBroadcast(Packet *frame) override;
+    virtual bool isAck(Packet *frame) override;
+
+};
+
+}
+} // namespace inet
+
+#endif
+
